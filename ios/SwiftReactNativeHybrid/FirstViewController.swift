4667a11129550bd9356074186249d795afbfa1e3
Jeremy Gale <jeremy@forcegrind.com>
2017-06-13 07:21:18 -0600

    Remove Obj-C based RN project, create brand new Swift project from Xcode

---
+++ ios/SwiftReactNativeHybrid/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftReactNativeHybrid
+//
+//  Created by Jeremy Gale on 2017-06-13.
+//  Copyright © 2017 Solium Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
