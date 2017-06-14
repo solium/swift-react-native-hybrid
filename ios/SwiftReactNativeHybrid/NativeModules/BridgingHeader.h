d991a08b0a0e17f0ef08a3bd6eab9b55c97f4eb6
Jeremy Gale <jeremy@forcegrind.com>
2017-06-13 21:37:10 -0600

    Update copyright to Solium Capital Inc.

--- ios/SwiftReactNativeHybrid/NativeModules/BridgingHeader.h
+++ ios/SwiftReactNativeHybrid/NativeModules/BridgingHeader.h
@@ -3,7 +3,7 @@
 //  SwiftReactNativeHybrid
 //
 //  Created by Jeremy Gale on 2017-06-13.
-//  Copyright © 2017 Solium Inc. All rights reserved.
+//  Copyright © 2017 Solium Capital Inc. All rights reserved.
 //
 
 #import <React/RCTBridgeModule.h>
--- ios/SwiftReactNativeHybridUITests/SwiftReactNativeHybridUITests.swift
+++ ios/SwiftReactNativeHybridUITests/SwiftReactNativeHybridUITests.swift
@@ -3,7 +3,7 @@
 //  SwiftReactNativeHybridUITests
 //
 //  Created by Jeremy Gale on 2017-06-13.
-//  Copyright © 2017 Solium Inc. All rights reserved.
+//  Copyright © 2017 Solium Capital Inc. All rights reserved.
 //
 
 import XCTest
